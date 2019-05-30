c76124893f0306eccea8b33115a48f1d0a4135dd
marsh22 <49203005+marsh22@users.noreply.github.com>
2019-05-30 14:44:37 +0300

    Update 2b.py

--- 2b.py
+++ 2b.py
@@ -1,8 +1,13 @@
 def geometric(l):
-    r = l[1] / l[0]
-    for i in range(1, len(l)-1, 1):
-       if l[i+1] / l[i] != r:
-           return False
-    return True
-
-print(geometric([44,22,11,5.5,2.75]))
+  f=0
+  r=l[1]/l[0]
+  for i in range(2,len(l)):
+    if (l[i]/l[i-1])!=r:
+      f=1
+      break
+  if f:
+    return False
+  return True
+print('Enter the list of numbers:')
+p=[int(x) for x in input().split()]
+print(geometric(p))
